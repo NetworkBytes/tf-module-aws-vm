a303d15e489331afe8bf8505082bf075951589d7
John Bencic <sales@networkbytes.com.au>
2017-10-24 19:04:42 +1100

    restructured to mimic the terraform registry AWS module

--- instances.tf
+++ instances.tf
@@ -1,24 +1,61 @@
 
-module "ec2_node" {
-  source = "terraform-aws-modules/ec2-instance/aws"
+#module "ec2_node" {
+#  source = "terraform-aws-modules/ec2-instance/aws"
+#
+#  name  = "${local.name}"
+#  count = "${local.vm_count}"
+#
+#  ami                    = "${local.ami_id}"
+#  instance_type          = "${local.instance_type}"
+#  #key_name               = "user1"
+#  monitoring             = false
+#  vpc_security_group_ids = [ "${local.vpc_security_group_ids}" ]
+#
+#  tags = {
+#    Terraform = "true"
+#    Environment = "${local.environment}"
+#  }
+#  associate_public_ip_address = true
+#  #subnet_id                   = "${local.subnet_id}"
+#  #user_data                   = "${data.template_file.user_data.rendered}"
+#
+#}
 
-  name  = "${local.instance_name}"
-  count = "${local.vm_count}"
-  
-  ami                    = "${local.ami_id}"
+######
+# EC2 instance
+######
+resource "aws_instance" "this" {
+  count = "${local.count}"
+
+  ami                    = "${local.ami}"
   instance_type          = "${local.instance_type}"
-  #key_name               = "user1"
-  monitoring             = false
-  vpc_security_group_ids = [ "${local.vpc_security_group_ids}" ]
+  user_data              = "${local.user_data}"
+  subnet_id              = "${local.subnet_id}"
+  key_name               = "${local.key_name}"
+  monitoring             = "${local.monitoring}"
+  vpc_security_group_ids = ["${local.vpc_security_group_ids}"]
+  iam_instance_profile   = "${local.iam_instance_profile}"
 
+  associate_public_ip_address = "${local.associate_public_ip_address}"
+  private_ip                  = "${local.private_ip}"
+  ipv6_address_count          = "${local.ipv6_address_count}"
+  ipv6_addresses              = "${local.ipv6_addresses}"
 
+  ebs_optimized          = "${local.ebs_optimized}"
+  volume_tags            = "${local.volume_tags}"
+  root_block_device      = "${local.root_block_device}"
+  ebs_block_device       = "${local.ebs_block_device}"
+  ephemeral_block_device = "${local.ephemeral_block_device}"
 
-  tags = {
-    Terraform = "true"
-    Environment = "${var.environment}"
-  }
-  associate_public_ip_address = true
-  #subnet_id                   = "${var.subnet_id}"
-  #user_data                   = "${data.template_file.user_data.rendered}"
+  source_dest_check                    = "${local.source_dest_check}"
+  disable_api_termination              = "${local.disable_api_termination}"
+  instance_initiated_shutdown_behavior = "${local.instance_initiated_shutdown_behavior}"
+  availability_zone                    = "${local.availability_zone}"
+  placement_group                      = "${local.placement_group}"
+  tenancy                              = "${local.tenancy}"
 
+  # Note: network_interface can't be specified together with associate_public_ip_address
+  # network_interface = "${local.network_interface}"
+
+  tags = "${merge(local.tags, map("Name", format("%s-%d", local.name, count.index+1)))}"
 }
--- locals.tf
+++ locals.tf
@@ -1,27 +1,52 @@
 locals {
 
-
-  vpc_security_group_ids_var = "${var.vpc_security_group_ids == "" 
-    ? data.aws_security_group.default.id : var.vpc_security_group_ids}"
-
-  vpc_security_group_ids = "${lookup(var.config, "vpc_security_group_ids", local.vpc_security_group_ids_var)}"
-
-  region        = "${lookup(var.config, "region", var.region)}"
-  vm_count      = "${lookup(var.config, "vm_count", var.vm_count)}"
-
-
   os            = "${lookup(var.config, "os", var.os)}"
-  instance_type = "${lookup(var.config, "instance_type", var.instance_type)}"
-
 
   ami_name      = "${lookup(var.config, "ami_name", lookup(var.ami_name, local.os))}"
-  ami_id        = "${data.aws_ami.ami.id}"
+  ami        = "${var.ami == "" ? data.aws_ami.ami.id : var.ami}"
 
 
-  # if instance_name is blank, use the "local.os" variable
-  instance_name = "${lookup(var.config, "instance_name", 
-                        var.instance_name == "" ? local.os : var.instance_name
-                    )}"
+  #vpc_security_group_ids_var = "${var.vpc_security_group_ids == ""
+  #  ? data.aws_security_group.default.id : var.vpc_security_group_ids}"
+
+  #vpc_security_group_ids = "${lookup(var.config, "vpc_security_group_ids", local.vpc_security_group_ids_var)}"
+
+  # if name is blank, default to using "local.os"
+  name_var = "${var.name == "" ? local.os : var.name}"
+  name = "${lookup(var.config, "name", local.name_var)}"
+
+  environment = "${lookup(var.config, "environment" , var.environment)}"
+  appid = "${lookup(var.config, "appid" , var.appid)}"
 
 
+  #########################################
+  # variables from AWS module
+  #########################################
+  #name = "${lookup(var.config, "name" , var.name)}"
+  count = "${lookup(var.config, "count" , var.count)}"
+  #ami = "${lookup(var.config, "ami" , var.ami)}"
+  availability_zone = "${lookup(var.config, "availability_zone" , var.availability_zone)}"
+  placement_group = "${lookup(var.config, "placement_group" , var.placement_group)}"
+  tenancy = "${lookup(var.config, "tenancy" , var.tenancy)}"
+  ebs_optimized = "${lookup(var.config, "ebs_optimized" , var.ebs_optimized)}"
+  disable_api_termination = "${lookup(var.config, "disable_api_termination" , var.disable_api_termination)}"
+  instance_initiated_shutdown_behavior = "${lookup(var.config, "instance_initiated_shutdown_behavior" , var.instance_initiated_shutdown_behavior)}"
+  instance_type = "${lookup(var.config, "instance_type" , var.instance_type)}"
+  key_name = "${lookup(var.config, "key_name" , var.key_name)}"
+  monitoring = "${lookup(var.config, "monitoring" , var.monitoring)}"
+  vpc_security_group_ids = "${lookup(var.config, "vpc_security_group_ids" , var.vpc_security_group_ids)}"
+  subnet_id = "${lookup(var.config, "subnet_id" , var.subnet_id)}"
+  associate_public_ip_address = "${lookup(var.config, "associate_public_ip_address" , var.associate_public_ip_address)}"
+  private_ip = "${lookup(var.config, "private_ip" , var.private_ip)}"
+  source_dest_check = "${lookup(var.config, "source_dest_check" , var.source_dest_check)}"
+  user_data = "${lookup(var.config, "user_data" , var.user_data)}"
+  iam_instance_profile = "${lookup(var.config, "iam_instance_profile" , var.iam_instance_profile)}"
+  ipv6_address_count = "${lookup(var.config, "ipv6_address_count" , var.ipv6_address_count)}"
+  ipv6_addresses = "${lookup(var.config, "ipv6_addresses" , var.ipv6_addresses)}"
+  tags = "${lookup(var.config, "tags" , var.tags)}"
+  volume_tags = "${lookup(var.config, "volume_tags" , var.volume_tags)}"
+  root_block_device = "${lookup(var.config, "root_block_device" , var.root_block_device)}"
+  ebs_block_device = "${lookup(var.config, "ebs_block_device" , var.ebs_block_device)}"
+  ephemeral_block_device = "${lookup(var.config, "ephemeral_block_device" , var.ephemeral_block_device)}"
+  network_interface = "${lookup(var.config, "network_interface" , var.network_interface)}"
 }
